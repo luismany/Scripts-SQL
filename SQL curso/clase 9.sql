27edebab33626d5def5128b90d9886f5a17028d9
luismany <teacher.luis360@gmail.com>
2023-01-11 22:00:42 -0600

    subiendo clase 9

---
+++ SQL curso/clase 9.sql
@@ -0,0 +1,49 @@
+ --constrain
+ if object_id('vehiculos') is not null
+  drop table vehiculos;
+
+ create table vehiculos(
+  patente char(6) not null,
+  tipo char(1),--'a'=auto, 'm'=moto
+  horallegada datetime not null,
+  horasalida datetime
+ );
+
+ alter table vehiculos
+ add constraint CK_vehiculos_tipo
+ check (tipo in ('a','m'));
+
+ alter table vehiculos
+  add constraint DF_vehiculos_tipo
+  default 'a'
+  for tipo;
+
+ alter table vehiculos
+  add constraint CK_vehiculos_patente_patron
+  check (patente like '[A-Z][A-Z][A-Z][0-9][0-9][0-9]');
+
+ alter table vehiculos
+  add constraint PK_vehiculos_patentellegada
+  primary key(patente,horallegada);
+
+ insert into vehiculos values('SDR456','a','2005/10/10 10:10',null);
+
+ insert into vehiculos values('SDR456','m','2005/10/10 10:10',null);
+
+ insert into vehiculos values('SDR456','m','2005/10/10 12:10',null);
+
+ insert into vehiculos values('SDR111','m','2005/10/10 10:10',null);
+
+ exec sp_helpconstraint vehiculos;
+
+ --elimina unconstrain
+ alter table vehiculos
+  drop DF_vehiculos_tipo;
+
+ exec sp_helpconstraint vehiculos;
+
+ alter table vehiculos
+  drop PK_vehiculos_patentellegada, CK_vehiculos_tipo;
+
+ exec sp_helpconstraint vehiculos;
+ ----------------------------------------------------------------------
